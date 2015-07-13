30f525fd54f469ded2d2dc8085dc2e8e2e0813bd
Chris Waring <chris@wwaves.co>
2015-07-14 00:48:25 +0100

    0.2.0

--- plugin/compile.js
+++ plugin/compile.js
@@ -1,26 +1,24 @@
 var path = Npm.require('path');
-var base = path.resolve('.');
-
-var basscssPath =
-  MeteorFilesHelpers.getNodeModulePath('kit:cssnext-basscss', 'basscss');
-
-var importDirs = [
-  path.join(basscssPath, 'node_modules'), // basscore,
-  path.resolve(basscssPath, '../') ] //basscss extensions
-
-var options = {
-  features: {
-    customProperties: {
-      strict: false // disable variable fallbacks from being redundantly added
-    },
-    rem: false,
-    pseudoElements: false,
-    colorRgba: false
-  },
-  import: { path: importDirs }
-};
 
 compileWithExtensions = function(compileStep, isLiterate) {
+  var basscssPath =
+    MeteorFilesHelpers.getNodeModulePath('kit:cssnext-basscss', 'basscss');
+
+  var importDirs = [
+    path.join(basscssPath, 'node_modules'), // basscore,
+    path.resolve(basscssPath, '../') ] //basscss extensions
+
+  var options = {
+    features: {
+      customProperties: {
+        strict: false // disable variable fallbacks from being redundantly added
+      },
+      rem: false,
+      pseudoElements: false,
+      colorRgba: false
+    },
+    import: { path: importDirs }
+  };
   return Cssnext.compile(compileStep, isLiterate, options)
 };
 
